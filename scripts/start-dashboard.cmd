b1005b11302a7b0b009185e03e5a4e6b16f03a3f
yygg693 <yygg693@users.noreply.github.com>
2026-08-01 09:30:31 +0800

    fix(scripts): start-dashboard.cmd use ping for sleep (timeout /t incompatible in git-bash)

--- scripts/start-dashboard.cmd
+++ scripts/start-dashboard.cmd
@@ -9,6 +9,6 @@ echo [autocomputer] Browser will open http://127.0.0.1:8765
 
 start "" cmd /c "cd /d %~dp0.. && set PYTHONPATH=python && python -c "import threading,webbrowser; from autocomputer.server import run_server; threading.Timer(2.0, lambda: webbrowser.open('http://127.0.0.1:8765')).start(); run_server()""
 
-timeout /t 4 >nul
+ping -n 5 127.0.0.1 >nul
 echo [autocomputer] If the browser did not open, visit http://127.0.0.1:8765 manually
 endlocal
